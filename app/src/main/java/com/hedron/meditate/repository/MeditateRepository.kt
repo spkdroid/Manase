edc3c22032545f4840f2092abbafa8299718e907
Ramkumar Velmurugan <vrk1990@outlook.com>
2020-07-05 11:52:56 -0400

    Piano and Veena implemented

--- app/src/main/java/com/hedron/meditate/repository/MeditateRepository.kt
+++ app/src/main/java/com/hedron/meditate/repository/MeditateRepository.kt
@@ -9,6 +9,8 @@ class MeditateRepository {
         listGuides.add(MeditationModel("Sleep", "Nap time is my happy hour", R.drawable.sleep_icon))
         listGuides.add(MeditationModel("Self care", "Giving yourself permission to pause", R.drawable.self_icon))
         listGuides.add(MeditationModel("Breathe", "Inhale peace and exhale worry", R.drawable.breath_icon))
+        listGuides.add(MeditationModel("Piano", "Music Speaks when word's can't", R.drawable.piano))
+        listGuides.add(MeditationModel("Veena", "SITAR The Essence of Indian music", R.drawable.veena))
         return listGuides
     }
 }
