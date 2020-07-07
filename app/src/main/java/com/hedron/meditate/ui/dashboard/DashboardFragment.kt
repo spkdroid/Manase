68a54f1727f3e37254445c65eaedec39eaea30a9
Ramkumar Velmurugan <vrk1990@outlook.com>
2020-07-06 22:19:49 -0400

    Dashboard fragment implemented

--- app/src/main/java/com/hedron/meditate/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/hedron/meditate/ui/dashboard/DashboardFragment.kt
@@ -1,5 +1,6 @@
 package com.hedron.meditate.ui.dashboard
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -30,6 +31,99 @@ class DashboardFragment : Fragment() {
         // })
         root.datetxt.text = getDateTime()
         root.clockView.setTime(10,10,10)
+
+        root.happyCardView.setOnClickListener {
+            root.happyCardView.setCardBackgroundColor(Color.parseColor("#C9E4DE"));
+            root.sadCardView.setCardBackgroundColor(Color.parseColor("#EDEDEB"));
+            root.angryCardView.setCardBackgroundColor(Color.parseColor("#EDEDEB"));
+            root.disgustCardView.setCardBackgroundColor(Color.parseColor("#EDEDEB"));
+            root.fearCardView.setCardBackgroundColor(Color.parseColor("#EDEDEB"));
+            root.loveCardView.setCardBackgroundColor(Color.parseColor("#EDEDEB"));
+            root.supriseCardView.setCardBackgroundColor(Color.parseColor("#EDEDEB"));
+            root.sickCardView.setCardBackgroundColor(Color.parseColor("#EDEDEB"));
+        }
+
+        root.sadCardView.setOnClickListener{
+            root.happyCardView.setCardBackgroundColor(Color.parseColor("#EDEDEB"));
+            root.sadCardView.setCardBackgroundColor(Color.parseColor("#C9E4DE"));
+            root.angryCardView.setCardBackgroundColor(Color.parseColor("#EDEDEB"));
+            root.disgustCardView.setCardBackgroundColor(Color.parseColor("#EDEDEB"));
+            root.fearCardView.setCardBackgroundColor(Color.parseColor("#EDEDEB"));
+            root.loveCardView.setCardBackgroundColor(Color.parseColor("#EDEDEB"));
+            root.supriseCardView.setCardBackgroundColor(Color.parseColor("#EDEDEB"));
+            root.sickCardView.setCardBackgroundColor(Color.parseColor("#EDEDEB"));
+        }
+
+        root.angryCardView.setOnClickListener{
+            root.happyCardView.setCardBackgroundColor(Color.parseColor("#EDEDEB"));
+            root.sadCardView.setCardBackgroundColor(Color.parseColor("#EDEDEB"));
+            root.angryCardView.setCardBackgroundColor(Color.parseColor("#C9E4DE"));
+            root.disgustCardView.setCardBackgroundColor(Color.parseColor("#EDEDEB"));
+            root.fearCardView.setCardBackgroundColor(Color.parseColor("#EDEDEB"));
+            root.loveCardView.setCardBackgroundColor(Color.parseColor("#EDEDEB"));
+            root.supriseCardView.setCardBackgroundColor(Color.parseColor("#EDEDEB"));
+            root.sickCardView.setCardBackgroundColor(Color.parseColor("#EDEDEB"));
+        }
+
+        root.disgustCardView.setOnClickListener{
+            root.happyCardView.setCardBackgroundColor(Color.parseColor("#EDEDEB"));
+            root.sadCardView.setCardBackgroundColor(Color.parseColor("#EDEDEB"));
+            root.angryCardView.setCardBackgroundColor(Color.parseColor("#EDEDEB"));
+            root.disgustCardView.setCardBackgroundColor(Color.parseColor("#C9E4DE"));
+            root.fearCardView.setCardBackgroundColor(Color.parseColor("#EDEDEB"));
+            root.loveCardView.setCardBackgroundColor(Color.parseColor("#EDEDEB"));
+            root.supriseCardView.setCardBackgroundColor(Color.parseColor("#EDEDEB"));
+            root.sickCardView.setCardBackgroundColor(Color.parseColor("#EDEDEB"));
+        }
+
+        root.fearCardView.setOnClickListener{
+            root.happyCardView.setCardBackgroundColor(Color.parseColor("#EDEDEB"));
+            root.sadCardView.setCardBackgroundColor(Color.parseColor("#EDEDEB"));
+            root.angryCardView.setCardBackgroundColor(Color.parseColor("#EDEDEB"));
+            root.disgustCardView.setCardBackgroundColor(Color.parseColor("#EDEDEB"));
+            root.fearCardView.setCardBackgroundColor(Color.parseColor("#C9E4DE"));
+            root.loveCardView.setCardBackgroundColor(Color.parseColor("#EDEDEB"));
+            root.supriseCardView.setCardBackgroundColor(Color.parseColor("#EDEDEB"));
+            root.sickCardView.setCardBackgroundColor(Color.parseColor("#EDEDEB"));
+        }
+
+        root.loveCardView.setOnClickListener{
+            root.happyCardView.setCardBackgroundColor(Color.parseColor("#EDEDEB"));
+            root.sadCardView.setCardBackgroundColor(Color.parseColor("#EDEDEB"));
+            root.angryCardView.setCardBackgroundColor(Color.parseColor("#EDEDEB"));
+            root.disgustCardView.setCardBackgroundColor(Color.parseColor("#EDEDEB"));
+            root.fearCardView.setCardBackgroundColor(Color.parseColor("#EDEDEB"));
+            root.loveCardView.setCardBackgroundColor(Color.parseColor("#C9E4DE"));
+            root.supriseCardView.setCardBackgroundColor(Color.parseColor("#EDEDEB"));
+            root.sickCardView.setCardBackgroundColor(Color.parseColor("#EDEDEB"));
+        }
+
+        root.supriseCardView.setOnClickListener{
+            root.happyCardView.setCardBackgroundColor(Color.parseColor("#EDEDEB"));
+            root.sadCardView.setCardBackgroundColor(Color.parseColor("#EDEDEB"));
+            root.angryCardView.setCardBackgroundColor(Color.parseColor("#EDEDEB"));
+            root.disgustCardView.setCardBackgroundColor(Color.parseColor("#EDEDEB"));
+            root.fearCardView.setCardBackgroundColor(Color.parseColor("#EDEDEB"));
+            root.loveCardView.setCardBackgroundColor(Color.parseColor("#EDEDEB"));
+            root.supriseCardView.setCardBackgroundColor(Color.parseColor("#C9E4DE"));
+            root.sickCardView.setCardBackgroundColor(Color.parseColor("#EDEDEB"));
+        }
+
+        root.sickCardView.setOnClickListener {
+            root.happyCardView.setCardBackgroundColor(Color.parseColor("#EDEDEB"));
+            root.sadCardView.setCardBackgroundColor(Color.parseColor("#EDEDEB"));
+            root.angryCardView.setCardBackgroundColor(Color.parseColor("#EDEDEB"));
+            root.disgustCardView.setCardBackgroundColor(Color.parseColor("#EDEDEB"));
+            root.fearCardView.setCardBackgroundColor(Color.parseColor("#EDEDEB"));
+            root.loveCardView.setCardBackgroundColor(Color.parseColor("#EDEDEB"));
+            root.supriseCardView.setCardBackgroundColor(Color.parseColor("#EDEDEB"));
+            root.sickCardView.setCardBackgroundColor(Color.parseColor("#C9E4DE"));
+        }
+
+
+
+
+
 
         return root
     }
