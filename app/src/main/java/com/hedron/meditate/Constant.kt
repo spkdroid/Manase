f9e44017d06c6851bc75657ba27adbc321b4231b
Ramkumar Velmurugan <vrk1990@outlook.com>
2020-07-06 23:27:24 -0400

    dashboard fragment implemented

---
+++ app/src/main/java/com/hedron/meditate/Constant.kt
@@ -0,0 +1,12 @@
+package com.hedron.meditate
+
+object Constant {
+    var Happy:String = "Happy"
+    var Sad:String = "Sad"
+    var Angry:String = "Angry"
+    var Disgust:String = "Disgust"
+    var Fear:String = "Fear"
+    var Love:String = "Love"
+    var Suprise:String = "Suprise"
+    var Sick:String = "Sick"
+}
--- app/src/main/java/com/hedron/meditate/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/hedron/meditate/ui/dashboard/DashboardFragment.kt
@@ -5,32 +5,39 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
+import com.hedron.meditate.Constant
 import com.hedron.meditate.R
+import kotlinx.android.synthetic.main.fragment_dashboard.*
 import kotlinx.android.synthetic.main.fragment_dashboard.view.*
 import java.text.DateFormat
 import java.util.*
+import java.util.concurrent.TimeUnit
 
 class DashboardFragment : Fragment() {
 
     private lateinit var dashboardViewModel: DashboardViewModel
 
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         dashboardViewModel =
-                ViewModelProviders.of(this).get(DashboardViewModel::class.java)
+            ViewModelProviders.of(this).get(DashboardViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_dashboard, container, false)
-       // val textView: TextView = root.findViewById(R.id.text_dashboard)
-       // dashboardViewModel.text.observe(viewLifecycleOwner, Observer {
-       //     textView.text = it
-       //
+        // val textView: TextView = root.findViewById(R.id.text_dashboard)
+        // dashboardViewModel.text.observe(viewLifecycleOwner, Observer {
+        //     textView.text = it
+        //
         // })
         root.datetxt.text = getDateTime()
-        root.clockView.setTime(10,10,10)
+
+        val milliseconds = System.currentTimeMillis()
+        root.clockView.setTime(TimeUnit.MILLISECONDS.toHours(milliseconds).toInt(), TimeUnit.MILLISECONDS.toMinutes(milliseconds).toInt(), TimeUnit.MILLISECONDS.toSeconds(milliseconds).toInt())
+        var selectedMood = "";
 
         root.happyCardView.setOnClickListener {
             root.happyCardView.setCardBackgroundColor(Color.parseColor("#C9E4DE"));
@@ -41,9 +48,10 @@ class DashboardFragment : Fragment() {
             root.loveCardView.setCardBackgroundColor(Color.parseColor("#EDEDEB"));
             root.supriseCardView.setCardBackgroundColor(Color.parseColor("#EDEDEB"));
             root.sickCardView.setCardBackgroundColor(Color.parseColor("#EDEDEB"));
+            selectedMood = Constant.Happy
         }
 
-        root.sadCardView.setOnClickListener{
+        root.sadCardView.setOnClickListener {
             root.happyCardView.setCardBackgroundColor(Color.parseColor("#EDEDEB"));
             root.sadCardView.setCardBackgroundColor(Color.parseColor("#C9E4DE"));
             root.angryCardView.setCardBackgroundColor(Color.parseColor("#EDEDEB"));
@@ -52,9 +60,10 @@ class DashboardFragment : Fragment() {
             root.loveCardView.setCardBackgroundColor(Color.parseColor("#EDEDEB"));
             root.supriseCardView.setCardBackgroundColor(Color.parseColor("#EDEDEB"));
             root.sickCardView.setCardBackgroundColor(Color.parseColor("#EDEDEB"));
+            selectedMood = Constant.Sad
         }
 
-        root.angryCardView.setOnClickListener{
+        root.angryCardView.setOnClickListener {
             root.happyCardView.setCardBackgroundColor(Color.parseColor("#EDEDEB"));
             root.sadCardView.setCardBackgroundColor(Color.parseColor("#EDEDEB"));
             root.angryCardView.setCardBackgroundColor(Color.parseColor("#C9E4DE"));
@@ -63,9 +72,10 @@ class DashboardFragment : Fragment() {
             root.loveCardView.setCardBackgroundColor(Color.parseColor("#EDEDEB"));
             root.supriseCardView.setCardBackgroundColor(Color.parseColor("#EDEDEB"));
             root.sickCardView.setCardBackgroundColor(Color.parseColor("#EDEDEB"));
+            selectedMood = Constant.Angry
         }
 
-        root.disgustCardView.setOnClickListener{
+        root.disgustCardView.setOnClickListener {
             root.happyCardView.setCardBackgroundColor(Color.parseColor("#EDEDEB"));
             root.sadCardView.setCardBackgroundColor(Color.parseColor("#EDEDEB"));
             root.angryCardView.setCardBackgroundColor(Color.parseColor("#EDEDEB"));
@@ -74,9 +84,10 @@ class DashboardFragment : Fragment() {
             root.loveCardView.setCardBackgroundColor(Color.parseColor("#EDEDEB"));
             root.supriseCardView.setCardBackgroundColor(Color.parseColor("#EDEDEB"));
             root.sickCardView.setCardBackgroundColor(Color.parseColor("#EDEDEB"));
+            selectedMood = Constant.Disgust
         }
 
-        root.fearCardView.setOnClickListener{
+        root.fearCardView.setOnClickListener {
             root.happyCardView.setCardBackgroundColor(Color.parseColor("#EDEDEB"));
             root.sadCardView.setCardBackgroundColor(Color.parseColor("#EDEDEB"));
             root.angryCardView.setCardBackgroundColor(Color.parseColor("#EDEDEB"));
@@ -85,9 +96,10 @@ class DashboardFragment : Fragment() {
             root.loveCardView.setCardBackgroundColor(Color.parseColor("#EDEDEB"));
             root.supriseCardView.setCardBackgroundColor(Color.parseColor("#EDEDEB"));
             root.sickCardView.setCardBackgroundColor(Color.parseColor("#EDEDEB"));
+            selectedMood = Constant.Fear
         }
 
-        root.loveCardView.setOnClickListener{
+        root.loveCardView.setOnClickListener {
             root.happyCardView.setCardBackgroundColor(Color.parseColor("#EDEDEB"));
             root.sadCardView.setCardBackgroundColor(Color.parseColor("#EDEDEB"));
             root.angryCardView.setCardBackgroundColor(Color.parseColor("#EDEDEB"));
@@ -96,9 +108,10 @@ class DashboardFragment : Fragment() {
             root.loveCardView.setCardBackgroundColor(Color.parseColor("#C9E4DE"));
             root.supriseCardView.setCardBackgroundColor(Color.parseColor("#EDEDEB"));
             root.sickCardView.setCardBackgroundColor(Color.parseColor("#EDEDEB"));
+            selectedMood = Constant.Love
         }
 
-        root.supriseCardView.setOnClickListener{
+        root.supriseCardView.setOnClickListener {
             root.happyCardView.setCardBackgroundColor(Color.parseColor("#EDEDEB"));
             root.sadCardView.setCardBackgroundColor(Color.parseColor("#EDEDEB"));
             root.angryCardView.setCardBackgroundColor(Color.parseColor("#EDEDEB"));
@@ -107,6 +120,7 @@ class DashboardFragment : Fragment() {
             root.loveCardView.setCardBackgroundColor(Color.parseColor("#EDEDEB"));
             root.supriseCardView.setCardBackgroundColor(Color.parseColor("#C9E4DE"));
             root.sickCardView.setCardBackgroundColor(Color.parseColor("#EDEDEB"));
+            selectedMood = Constant.Suprise
         }
 
         root.sickCardView.setOnClickListener {
@@ -118,12 +132,18 @@ class DashboardFragment : Fragment() {
             root.loveCardView.setCardBackgroundColor(Color.parseColor("#EDEDEB"));
             root.supriseCardView.setCardBackgroundColor(Color.parseColor("#EDEDEB"));
             root.sickCardView.setCardBackgroundColor(Color.parseColor("#C9E4DE"));
+            selectedMood = Constant.Sick
         }
 
 
-
-
-
+        root.saveCard.setOnClickListener {
+            if(selectedMood.isNotEmpty() && dialogEditTxt.text!!.isNotEmpty()) {
+                Toast.makeText(requireContext(),selectedMood,Toast.LENGTH_LONG).show()
+                Toast.makeText(requireContext(),dialogEditTxt.text,Toast.LENGTH_LONG).show()
+            } else {
+                Toast.makeText(requireContext(),"Input Error",Toast.LENGTH_LONG).show()
+            }
+        }
 
         return root
     }
@@ -132,5 +152,4 @@ class DashboardFragment : Fragment() {
         val calendar = Calendar.getInstance()
         return DateFormat.getDateInstance(DateFormat.FULL).format(calendar.time)
     }
-
 }
